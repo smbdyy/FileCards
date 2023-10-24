2a6fee2c1835d089d35e2a71abf087a1937ee737
Ilya <ividivi67@gmail.com>
2023-10-24 20:51:43 +0300

    add FileCard and TxtFileCard

---
+++ Domain/Files/FileCard.cs
@@ -0,0 +1,24 @@
+﻿namespace FileCards.Domain;
+
+public abstract class FileCard
+{
+    protected FileCard(string path)
+    {
+        Path = ValidateExtension(path);
+    }
+    
+    public string Description { get; set; } = string.Empty;
+
+    public string Path { get; protected set; }
+    public abstract string Extension { get; }
+
+    private string ValidateExtension(string path)
+    {
+        if (System.IO.Path.GetExtension(path) == Extension)
+        {
+            return path;
+        }
+
+        throw new NotImplementedException();
+    }
+}
---
+++ Domain/Files/TxtFileCard.cs
@@ -0,0 +1,8 @@
+﻿namespace FileCards.Domain;
+
+public class TxtFileCard : FileCard
+{
+    public TxtFileCard(string path) : base(path) { }
+
+    public override string Extension => "txt";
+}
