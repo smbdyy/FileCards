f9671ecb2e2e73c3ee3a4cbf461b9a2c2bfc3920
Ilya <ividivi67@gmail.com>
2023-10-28 03:33:57 +0300

    refactor description validation

--- Domain/Files/FileCard.cs
+++ Domain/Files/FileCard.cs
@@ -7,18 +7,27 @@ public class FileCard
     public static readonly string[] AllowedExtensions = { ".txt", ".docx", ".pdf" };
     public const int MaxDescriptionLength = 500;
 
+    private string _description = null!;
+
     public FileCard(string name, string description)
     {
         Name = ValidateExtension(name);
-        if (description.Length > MaxDescriptionLength)
-        {
-            throw ValidationException.DescriptionTooLong();
-        }
-        
         Description = description;
     }
     
-    public string Description { get; set; }
+    public string Description
+    {
+        get => _description;
+        set
+        {
+            if (value.Length > MaxDescriptionLength)
+            {
+                throw ValidationException.DescriptionTooLong();
+            }
+
+            _description = value;
+        }
+    }
     public string Name { get; private set; }
 
     private static string ValidateExtension(string name)
