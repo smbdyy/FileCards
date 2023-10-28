aff40dbb8de10a3451d1a0ae3c5db63cacc16e59
Ilya <ividivi67@gmail.com>
2023-10-28 04:57:07 +0300

    add get files handlers

---
+++ Application/RequestHandlers/Files/GetAllFilesHandler.cs
@@ -0,0 +1,22 @@
+﻿using FileCards.Application.Abstractions.DataAccess;
+using FileCards.Application.Mapping;
+using MediatR;
+
+using static Application.Contracts.GetAllFiles;
+
+namespace FileCards.Application.RequestHandlers.Files;
+
+internal class GetAllFilesHandler : IRequestHandler<Request, Response>
+{
+    private readonly IFileCardsDbContext _context;
+
+    public GetAllFilesHandler(IFileCardsDbContext context)
+    {
+        _context = context;
+    }
+    
+    public Task<Response> Handle(Request request, CancellationToken cancellationToken)
+    {
+        return Task.FromResult(new Response(_context.FileCards.Select(card => card.AsDto())));
+    }
+}
---
+++ Application/RequestHandlers/Files/GetFileByNameHandler.cs
@@ -0,0 +1,25 @@
+﻿using FileCards.Application.Abstractions.DataAccess;
+using FileCards.Application.Extensions;
+using FileCards.Application.Mapping;
+using MediatR;
+
+using static Application.Contracts.GetFileByName;
+
+namespace FileCards.Application.RequestHandlers.Files;
+
+internal class GetFileByNameHandler : IRequestHandler<Request, Response>
+{
+    private readonly IFileCardsDbContext _context;
+
+    public GetFileByNameHandler(IFileCardsDbContext context)
+    {
+        _context = context;
+    }
+    
+    public async Task<Response> Handle(Request request, CancellationToken cancellationToken)
+    {
+        var fileCard = await _context.FileCards.GetByFilenameAsync(request.Filename, cancellationToken);
+
+        return new Response(fileCard.AsDto());
+    }
+}
