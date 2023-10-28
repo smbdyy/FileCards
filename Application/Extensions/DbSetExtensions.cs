f76529144dc586a44c91c0d9ac90fbeb7d6d7164
Ilya <ividivi67@gmail.com>
2023-10-28 03:35:01 +0300

    add ChangeFileDescriptionHandler

---
+++ Application/Extensions/DbSetExtensions.cs
@@ -0,0 +1,22 @@
+﻿using FileCards.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace FileCards.Application.Extensions;
+
+internal static class DbSetExtensions
+{
+    public static async Task<FileCard> GetByFilenameAsync(
+        this DbSet<FileCard> fileCards,
+        string filename,
+        CancellationToken cancellationToken)
+    {
+        var card = await fileCards.FindAsync(new object[] { filename }, cancellationToken);
+
+        if (card == null)
+        {
+            throw new NotImplementedException();
+        }
+
+        return card;
+    }
+}
---
+++ Application/RequestHandlers/Files/ChangeFileDescriptionHandler.cs
@@ -0,0 +1,27 @@
+﻿using FileCards.Application.Abstractions.DataAccess;
+using FileCards.Application.Extensions;
+using MediatR;
+
+using static Application.Contracts.ChangeFileDescription;
+
+namespace FileCards.Application.RequestHandlers.Files;
+
+internal class ChangeFileDescriptionHandler : IRequestHandler<Request>
+{
+    private readonly IFileCardsDbContext _context;
+
+    public ChangeFileDescriptionHandler(IFileCardsDbContext context)
+    {
+        _context = context;
+    }
+    
+    public async Task Handle(Request request, CancellationToken cancellationToken)
+    {
+        var fileCard = await _context.FileCards.GetByFilenameAsync(request.Filename, cancellationToken);
+
+        fileCard.Description = request.NewDescription;
+        _context.FileCards.Update(fileCard);
+
+        await _context.SaveChangesAsync(cancellationToken);
+    }
+}
