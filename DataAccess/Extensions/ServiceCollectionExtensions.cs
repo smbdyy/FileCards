c5f661f3b8a5c95e672acd417e05affa3dd73f13
Ilya <ividivi67@gmail.com>
2023-10-28 21:47:35 +0300

    add AddDataAccess extension

---
+++ DataAccess/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,18 @@
+﻿using FileCards.Application.Abstractions.DataAccess;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace FileCards.DataAccess;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddDataAccess(
+        this IServiceCollection services,
+        Action<DbContextOptionsBuilder> configuration)
+    {
+        services.AddDbContext<FileCardsDbContext>(configuration);
+        services.AddScoped<IFileCardsDbContext>(provider => provider.GetRequiredService<FileCardsDbContext>());
+
+        return services;
+    }
+}
