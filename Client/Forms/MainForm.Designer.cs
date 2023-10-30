d9c97b00dcddc9d071b433f0e561a13ccc410d66
Ilya <ividivi67@gmail.com>
2023-10-30 19:13:49 +0300

    add MainForm

---
+++ Client/Forms/MainForm.Designer.cs
@@ -0,0 +1,74 @@
+﻿using System.ComponentModel;
+
+namespace Client.Forms;
+
+partial class MainForm
+{
+    private System.ComponentModel.IContainer components = null;
+
+    protected override void Dispose(bool disposing)
+    {
+        if (disposing && (components != null))
+        {
+            components.Dispose();
+        }
+
+        base.Dispose(disposing);
+    }
+    
+    private VScrollBar _filesGridScrollBar;
+    private DataGridView _filesDataGrid;
+    private Button _addFileButton;
+    
+    private void InitializeComponent()
+    {
+        _addFileButton = new Button();
+        _filesDataGrid = new DataGridView();
+        _filesGridScrollBar = new VScrollBar();
+        ((System.ComponentModel.ISupportInitialize)(_filesDataGrid)).BeginInit();
+        SuspendLayout();
+        // 
+        // _addFileButton
+        // 
+        _addFileButton.Location = new System.Drawing.Point(13, 22);
+        _addFileButton.Name = "_addFileButton";
+        _addFileButton.Size = new System.Drawing.Size(233, 33);
+        _addFileButton.TabIndex = 0;
+        _addFileButton.Text = "Добавить файл";
+        _addFileButton.UseVisualStyleBackColor = true;
+        // 
+        // _filesDataGrid
+        // 
+        _filesDataGrid.AllowUserToAddRows = false;
+        _filesDataGrid.AllowUserToDeleteRows = false;
+        _filesDataGrid.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+        _filesDataGrid.Location = new System.Drawing.Point(13, 82);
+        _filesDataGrid.Name = "_filesDataGrid";
+        _filesDataGrid.ReadOnly = true;
+        _filesDataGrid.RowTemplate.Height = 24;
+        _filesDataGrid.Size = new System.Drawing.Size(336, 367);
+        _filesDataGrid.TabIndex = 1;
+        // 
+        // _filesGridScrollBar
+        // 
+        _filesGridScrollBar.Location = new System.Drawing.Point(352, 82);
+        _filesGridScrollBar.Name = "_filesGridScrollBar";
+        _filesGridScrollBar.Size = new System.Drawing.Size(23, 367);
+        _filesGridScrollBar.TabIndex = 2;
+        // 
+        // Form
+        // 
+        AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+        AutoScaleMode = AutoScaleMode.Font;
+        ClientSize = new System.Drawing.Size(392, 476);
+        Controls.Add(_filesGridScrollBar);
+        Controls.Add(_filesDataGrid);
+        Controls.Add(_addFileButton);
+        Name = "MainForm";
+        Text = "FileCards";
+        Load += FormLoaded;
+
+        ((System.ComponentModel.ISupportInitialize)(_filesDataGrid)).EndInit();
+        ResumeLayout(false);
+    }
+}
---
+++ Client/Forms/MainForm.cs
@@ -0,0 +1,44 @@
+﻿using System.Windows.Forms;
+using Application.Contracts;
+using MediatR;
+
+namespace Client.Forms;
+
+public partial class MainForm : Form
+{
+    private readonly IMediator _mediator;
+    
+    public MainForm(IMediator mediator)
+    {
+        _mediator = mediator;
+        InitializeComponent();
+    }
+
+    private async void FormLoaded(object sender, EventArgs e)
+    {
+        var response = await _mediator.Send(new GetAllFiles.Request());
+        var fileCards = response.Files;
+    }
+
+    private async void AddFileButtonClicked(object sender, EventArgs e)
+    {
+        using var dialog = CreateFilePickingDialog();
+        if (dialog.ShowDialog() != DialogResult.OK) return;
+
+        var filepath = dialog.FileName;
+        
+    }
+
+    private static OpenFileDialog CreateFilePickingDialog()
+    {
+        var dialog = new OpenFileDialog();
+        dialog.AddExtension = true;
+        dialog.CheckFileExists = true;
+        dialog.CheckPathExists = true;
+        dialog.Multiselect = true;
+        dialog.SelectReadOnly = true;
+        dialog.Filter = "PDF Files (*.pdf)|*.pdf|Word Files (*.docx)|*.docx|Text Files (*.txt)|*.txt";
+
+        return dialog;
+    }
+}
